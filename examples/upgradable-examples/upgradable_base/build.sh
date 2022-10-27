5c8f7548888eaa6f0c2d2cb10b28604a1e9c9c49
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2022-10-27 11:32:23 +0300

    copy wasm file to res folder

--- examples/upgradable-examples/upgradable_base/build.sh
+++ examples/upgradable-examples/upgradable_base/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+mkdir -p ../../res
+
 RUSTFLAGS='-C link-arg=-s' cargo build --features "counter2" --target wasm32-unknown-unknown --release
 mv  ../../target/wasm32-unknown-unknown/release/upgradable_base.wasm ../../target/wasm32-unknown-unknown/release/upgradable_base_second.wasm
 RUSTFLAGS='-C link-arg=-s' cargo build --features "counter1" --target wasm32-unknown-unknown --release
+
+cp ../../target/wasm32-unknown-unknown/release/upgradable_base_second.wasm ../../res/
+cp ../../target/wasm32-unknown-unknown/release/upgradable_base.wasm ../../res/
