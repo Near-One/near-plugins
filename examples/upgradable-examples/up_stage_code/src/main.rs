b8b88ae3b5e8b5b65f2ac101f79119d30b68122f
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2022-11-03 21:18:06 +0200

    use async main

--- examples/upgradable-examples/up_stage_code/src/main.rs
+++ examples/upgradable-examples/up_stage_code/src/main.rs
@@ -1,14 +1,13 @@
 use std::env;
-use tokio::runtime::Runtime;
 use workspaces::Account;
 
 const WASM_FILEPATH_SECOND: &str =
     "../../target/wasm32-unknown-unknown/release/upgradable_base_second.wasm";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
-    let rt = Runtime::new().unwrap();
-    let worker = rt.block_on(workspaces::testnet()).unwrap();
+    let worker = workspaces::testnet().await.unwrap();
 
     let contract: Account = Account::from_file(args[1].clone(), &worker).unwrap();
 
@@ -16,14 +15,13 @@ fn main() {
 
     println!(
         "{}",
-        rt.block_on(
-            contract
-                .call(contract.id(), "up_stage_code")
-                .args_borsh(wasm)
-                .max_gas()
-                .transact()
-        )
-        .unwrap()
-        .is_success()
+        contract
+            .call(contract.id(), "up_stage_code")
+            .args_borsh(wasm)
+            .max_gas()
+            .transact()
+            .await
+            .unwrap()
+            .is_success()
     );
 }
