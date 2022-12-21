ff5ecbc15552b920ccc9c42f4a89ef2162061f6d
Moritz <moritz.zielke@gmail.com>
2022-12-21 18:14:08 +0100

    Remove examples/pausable-examples

--- examples/pausable-examples/pausable_base/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ../../res
-
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-
-cp ../../target/wasm32-unknown-unknown/release/pausable_base.wasm ../../res/
