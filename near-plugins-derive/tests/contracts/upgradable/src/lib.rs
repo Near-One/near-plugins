1f1a40edd2137d233764429b7964d9f22f8c0308
Moritz <moritz.zielke@gmail.com>
2023-03-01 16:15:32 +0100

    test: update test/demo contracts

--- near-plugins-derive/tests/contracts/upgradable/src/lib.rs
+++ near-plugins-derive/tests/contracts/upgradable/src/lib.rs
@@ -1,14 +1,37 @@
-use near_plugins::{Ownable, Upgradable};
+use near_plugins::{access_control, AccessControlRole, AccessControllable, Upgradable};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{near_bindgen, AccountId, Duration, PanicOnDefault};
 
-/// Deriving `Upgradable` requires the contract to be `Ownable.`
+// TODO add doc comments
+#[derive(AccessControlRole, Deserialize, Serialize, Copy, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub enum Role {
+    // May do anything
+    DAO,
+    CodeStager,
+    CodeDeployer,
+    DurationManager,
+}
+
+/// Deriving `Upgradable` requires the contract to be `AccessControllable`.
+#[access_control(role_type(Role))]
 #[near_bindgen]
-#[derive(Ownable, Upgradable, PanicOnDefault, BorshDeserialize, BorshSerialize)]
-pub struct Contract;
+#[derive(Upgradable, PanicOnDefault, BorshDeserialize, BorshSerialize)]
+#[upgradable(access_control_roles(
+    code_stagers(Role::CodeStager, Role::DAO),
+    code_deployers(Role::CodeDeployer, Role::DAO),
+    duration_initializers(Role::DurationManager, Role::DAO),
+    duration_update_stagers(Role::DurationManager, Role::DAO),
+    duration_update_appliers(Role::DurationManager, Role::DAO),
+))]
+pub struct Contract {
+    dummy: u64, // TODO remove after `__acl` field was removed (#84)
+}
 
 #[near_bindgen]
 impl Contract {
+    // TODO update docs and comments
     /// Parameter `owner` allows setting the owner in the constructor if an `AccountId` is provided.
     /// If `owner` is `None`, no owner will be set in the constructor. After contract initialization
     /// it is possible to set an owner with `Ownable::owner_set`.
@@ -23,12 +46,18 @@ impl Contract {
     /// Since this constructor uses an `*_unchecked` method, it should be combined with code
     /// deployment in a batch transaction.
     #[init]
-    pub fn new(owner: Option<AccountId>, staging_duration: Option<Duration>) -> Self {
-        let mut contract = Self;
+    pub fn new(dao: Option<AccountId>, staging_duration: Option<Duration>) -> Self {
+        let mut contract = Self {
+            dummy: 0,
+            __acl: Default::default(), // TODO remove after merging #84
+        };
 
-        // Optionally set the owner.
-        if owner.is_some() {
-            contract.owner_set(owner);
+        if let Some(account_id) = dao {
+            // TODO add warning regarding `*_unchecked()`
+            let res = contract
+                .__acl
+                .grant_role_unchecked(Role::DAO.into(), &account_id);
+            assert_eq!(true, res, "Failed to grant role");
         }
 
         // Optionally initialize the staging duration.
@@ -41,6 +70,7 @@ impl Contract {
             // by access control. Otherwise there is a risk of unwanted state changes carried out by
             // malicious users. For this example, we assume the constructor is called in a batch
             // transaction together with code deployment.
+            // TODO try using up_init_staging_duration
             contract.up_set_staging_duration_unchecked(staging_duration);
         }
 
