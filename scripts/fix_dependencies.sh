7c3a7139a8c7371690795fcba2711fe49559766d
Moritz <moritz.zielke@aurora.dev>
2023-11-14 12:48:12 +0100

    Fix version of dependency to downgrade

--- scripts/fix_dependencies.sh
+++ scripts/fix_dependencies.sh
@@ -13,5 +13,5 @@
 # for some other attempts and how they failed in CI).
 cargo update -p anstyle@1.0.4 --precise 1.0.2
 cargo update -p anstyle-parse@0.2.2 --precise 0.2.1
-cargo update -p clap@4.4.7 --precise 4.3.24
+cargo update -p clap@4.4.8 --precise 4.3.24
 cargo update -p clap_lex@0.5.1 --precise 0.5.0
