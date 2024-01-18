9374fcc560a260f493cb50907040cd5925121652
Moritz Zielke <moritz.zielke@aurora.dev>
2024-01-18 22:03:08 +0100

    build: add workarounds required to build tests (#128)
    * build: patch a yanked near-sdk dependency
    
    * Update `scripts/fix_dependencies`

--- scripts/fix_dependencies.sh
+++ scripts/fix_dependencies.sh
@@ -11,7 +11,12 @@
 # version of `clap` is released, say 4.4.8, then below must be changed to `-p clap@4.4.8`. Even
 # though this requires maintenance, it seems to be cleanest approach that works with CI (see #119
 # for some other attempts and how they failed in CI).
+cargo update -p ahash@0.8.7 --precise 0.8.4
 cargo update -p anstyle@1.0.4 --precise 1.0.2
-cargo update -p anstyle-parse@0.2.2 --precise 0.2.1
-cargo update -p clap@4.4.8 --precise 4.3.24
+cargo update -p anstyle-parse@0.2.3 --precise 0.2.1
+cargo update -p anstyle-query@1.0.2 --precise 1.0.0
+cargo update -p cargo-platform@0.1.6 --precise 0.1.5
+cargo update -p clap@4.4.18 --precise 4.3.24
 cargo update -p clap_lex@0.5.1 --precise 0.5.0
+cargo update -p colored@2.1.0 --precise 2.0.4
+cargo update -p home@0.5.9 --precise 0.5.5
